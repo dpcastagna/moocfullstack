ee75bf14664a21ac3808211b055f56d39355e381
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-04-07 22:14:37 +0300

    step1.7

--- osa1/unicafe/src/App.js
+++ osa1/unicafe/src/App.js
@@ -9,20 +9,24 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const handleGood = () => {    
     //setAll(allClicks.concat('G'))    
     setGood(good + 1)  
+    setAll(all + 1)
   }
 
   const handleNeutral = () => {    
     //setAll(allClicks.concat('N'))    
     setNeutral(neutral + 1)  
+    setAll(all + 1)
   }
 
   const handleBad = () => {    
     //setAll(allClicks.concat('B'))    
     setBad(bad + 1)  
+    setAll(all + 1)
   }
 
   return (
@@ -35,6 +39,9 @@ const App = () => {
       good {good} <br />
       neutral {neutral} <br />
       bad {bad} <br />
+      all {all} <br />
+      average {(good+bad*-1)/all} <br />
+      positive {100*good/all} %<br />
     </div>
   )
 }
