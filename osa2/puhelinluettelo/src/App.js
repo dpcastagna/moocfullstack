59f5bd3e2f68838a78527fa14f35c94b34331d18
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-04-20 20:54:49 +0300

    2.10

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,52 @@
 import { useState } from 'react'
 
+const Filter = (props) => {
+  //console.log(props)
+
+  return(
+    <div>
+    filter shown with <input 
+                  value={props.filter}
+                  onChange={props.handleFilter}
+                />
+    </div>
+  )
+}
+
+const PersonForm = (props) => {
+  return(
+    <div>
+      <form onSubmit={props.submit}>
+        <div>
+          name: <input 
+                  value={props.name}
+                  onChange={props.handleName}
+                />
+        </div>
+        <div>
+          number: <input 
+                  value={props.number}
+                  onChange={props.handleNumber}
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+const Persons = (props) => {
+  return(
+    <div>
+      {props.persons.map(person =>
+        <p>{person.name} {person.number}</p>
+      )}
+    </div>
+  )
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-123456' },
@@ -64,34 +111,17 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-      <div>
-          filter shown with <input 
-                  value={newFilter}
-                  onChange={handleFilterChange}
-                />
-        </div>
-      <h2>add a new</h2>
-      <form onSubmit={addName}>
-        <div>
-          name: <input 
-                  value={newName}
-                  onChange={handleNameChange}
-                />
-        </div>
-        <div>
-          number: <input 
-                  value={newNumber}
-                  onChange={handleNumberChange}
-                />
-        </div>
-        <div>
-          <button type="submit">add</button>
-        </div>
-      </form>
+      <Filter handleFilter={handleFilterChange} filter={newFilter}/>
+      
+      <h2>Add a new</h2>
+      <PersonForm submit={addName} 
+                  name={newName} 
+                  handleName={handleNameChange} 
+                  number={newNumber}
+                  handleNumber={handleNumberChange} />
+      
       <h2>Numbers</h2>
-      {personsToShow.map(person =>
-        <p>{person.name} {person.number}</p>
-      )}
+      <Persons persons={personsToShow} />
     </div>
   )
 
