9b378424ddfe8660059b80924f6e9446ff44e80c
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-04-26 20:44:02 +0300

    3.3

--- osa3/index.js
+++ osa3/index.js
@@ -24,15 +24,27 @@ let persons = [
     }
 ]
 
-/*app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
-})*/
+app.get('/info', (req, res) => {
+    res.send(`<p>Phonebook has info for ${persons.length} people</p>
+                <p>${Date()}</p>`)
+})
   
 app.get('/api/persons', (req, res) => {
     res.json(persons)
 })
 
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(note => note.id === id)
+    if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+  })
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
+    //console.log(Math.floor(Math.random() * 1000000000000000))
 })
