695cb4c798815622f6adca92241322677e230abd
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-04-28 21:16:17 +0300

    3.6

--- osa3/index.js
+++ osa3/index.js
@@ -56,6 +56,20 @@ app.post('/api/persons', (request, response) => {
   const person = request.body
   person.id = Math.floor(Math.random() * 1000000000000000)
   console.log(person)
+  if (person.name === "") {
+    return response.status(403).json({ 
+      error: 'name missing' 
+    })
+  } else if (person.number === "") {
+    return response.status(403).json({ 
+      error: 'number missing' 
+    })
+  } else if(persons.filter(p => p.name === person.name).length > 0) {
+    //console.log(persons.filter(p => p.name === person.name).length)
+    return response.status(403).json({ 
+      error: 'name must be unique' 
+    })
+  }
   persons = persons.concat(person)
   response.json(person)
 })
