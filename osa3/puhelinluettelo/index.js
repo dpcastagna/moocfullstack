54bbe42eb82fb4f1f6e67551e18e7a7ab14abe28
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-05-10 00:17:20 +0300

    3.19

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -55,7 +55,7 @@ app.delete('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
   const body = request.body
   //person.id = Math.floor(Math.random() * 1000000000000000)
   //console.log(person)
@@ -77,9 +77,11 @@ app.post('/api/persons', (request, response) => {
     name: body.name,
     number: body.number,
   })
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
-  })
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
@@ -108,6 +110,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
