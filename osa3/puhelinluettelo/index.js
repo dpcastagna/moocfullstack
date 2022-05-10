37b704dc6aa7791cd5267549d3a2534865209c68
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-05-10 18:30:56 +0300

    3.22

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -16,23 +16,23 @@ const requestLogger = (request, response, next) => {
 app.use(express.static('build'))
 app.use(express.json())
 app.use(cors())
-//app.use(requestLogger)
+app.use(requestLogger)
 morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
 
 app.get('/info', (request, response) => {
-    Person.find({}).then(persons => {
+  Person.find({}).then(persons => {
     response.send(`<p>Phonebook has info for ${persons.length} people</p>
                 <p>${Date()}</p>`)
-    })
+  })
 })
-  
+
 app.get('/api/persons', (request, response) => {
-    Person.find({}).then(persons => {
-      response.json(persons)
-    })
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -59,18 +59,18 @@ app.post('/api/persons', (request, response, next) => {
   const body = request.body
   //person.id = Math.floor(Math.random() * 1000000000000000)
   //console.log(person)
-  if (body.name === "") {
-    return response.status(403).json({ 
-      error: 'name missing' 
+  if (body.name === '') {
+    return response.status(403).json({
+      error: 'name missing'
     })
-  } else if (body.number === "") {
-    return response.status(403).json({ 
-      error: 'number missing' 
+  } else if (body.number === '') {
+    return response.status(403).json({
+      error: 'number missing'
     })
   } /*else if(persons.filter(p => p.name === person.name).length > 0) {
     //console.log(persons.filter(p => p.name === person.name).length)
-    return response.status(403).json({ 
-      error: 'name must be unique' 
+    return response.status(403).json({
+      error: 'name must be unique'
     })
   }*/
   const person = new Person({
@@ -122,5 +122,5 @@ app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
