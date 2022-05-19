3bf23f35dc0987355675de0a1a346b145f11a024
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-05-19 21:11:16 +0300

    4.19

--- osa4/blogilista/controllers/users.js
+++ osa4/blogilista/controllers/users.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
+const Blog = require('../models/blog')
 const User = require('../models/user')
 
 usersRouter.post('/', async (request, response) => {
@@ -27,7 +28,8 @@ usersRouter.post('/', async (request, response) => {
 })
 
 usersRouter.get('/', async (request, response) => {
-    const users = await User.find({})
+    const users = await User
+      .find({}).populate('blogs', { url: 1, title: 1, author: 1, id: 1 })
     response.json(users)
 })
 
