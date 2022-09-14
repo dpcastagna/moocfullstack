b2a2bd998c958d48f951a6562b2fe59c62ecdf67
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-09-14 18:05:03 +0300

    linting fixed hopefully

--- osa3/models/person.js
+++ osa3/models/person.js
@@ -18,9 +18,10 @@ const personSchema = new mongoose.Schema({
   },
   number: {
     type: String,
+    minlength: 8,
     validate : {
       validator: function(v) {
-        return /\d{2,3}-\d{7,8}$/.test(v);
+        return /\d{2,3}-\d{7,8}$/.test(v)
       },
       message: props => `${props.value} is not a valid phone number!`
     },
