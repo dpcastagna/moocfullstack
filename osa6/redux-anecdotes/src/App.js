8ee5d05c38085d489c375de352bbfbabe5d83112
dpcastagna <99268971+dpcastagna@users.noreply.github.com>
2022-12-16 21:23:31 +0200

    6.5

---
+++ osa6/redux-anecdotes/src/App.js
@@ -0,0 +1,54 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+
+    dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch({
+      type: 'NEW_ANECDOTE',
+      data: { 
+        content: content,
+        votes: 0
+      }
+    })
+  }
+  console.log(anecdotes)
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.sort((a, b) => {
+        return a.votes > b.votes ? -1 : 1
+      }).map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
