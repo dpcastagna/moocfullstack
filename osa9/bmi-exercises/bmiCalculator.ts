3c11783260c15fabff7987d681bd6a7dd1d93f0f
Daniele Castagna <daniele.castagna@outlook.com>
2023-07-24 12:22:04 +0300

    9.2-9.3

--- osa9/bmi-exercises/bmiCalculator.ts
+++ osa9/bmi-exercises/bmiCalculator.ts
@@ -1,18 +1,17 @@
 interface BmiValues {
-  mass: number;
   height: number;
+  mass: number;
 }
-
-// type Operation = 'multiply' | 'add' | 'divide';
 
 const parseArguments = (args: string[]): BmiValues => {
   if (args.length < 4) throw new Error('Not enough arguments');
   if (args.length > 4) throw new Error('Too many arguments');
+  // console.log(args.slice(2).map(Number));
 
   if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
     return {
-      mass: Number(args[2]),
-      height: Number(args[3])
+      height: Number(args[2]),
+      mass: Number(args[3])
     }
   } else {
     throw new Error('Provided values were not numbers!');
@@ -20,22 +19,23 @@ const parseArguments = (args: string[]): BmiValues => {
 }
 
 const calculateBmi = (height: number, mass: number) : string => {
-    const bmi = mass / ((height / 100) * (height / 100));
-    console.log(bmi)
+    const bmi = mass / ((height /100) * (height / 100));
+    // console.log(bmi)
     if (bmi < 18.5) {
-      return 'Underweight (Unhealthy weight)';
+      return 'Underweight';
     } else if (bmi < 25) {
       return 'Normal (Healthy weight)';
     } else if (bmi < 30) {
-      return 'Overweight (Unhealthy weight)';
+      return 'Overweight';
     } else {
-      return 'Obese (Unhealthy weight)';
+      return 'Obese';
     }
 }
 
 try {
-  // console.log(calculateBmi(155, 55));
-  console.log(calculateBmi(180, 74));
+  const { height, mass } = parseArguments(process.argv);
+  // console.log(height, mass)
+  console.log(calculateBmi(height, mass));
 } catch (error: unknown) {
   let errorMessage = 'Something went wrong: '
   if (error instanceof Error) {
@@ -43,3 +43,5 @@ try {
   }
   console.log(errorMessage);
 }
+
+export {}
--- osa9/bmi-exercises/exerciseCalculator.ts
+++ osa9/bmi-exercises/exerciseCalculator.ts
@@ -1,6 +1,8 @@
+type ratingRange = 1 | 2 | 3;
+
 interface exerciseValues {
   target: number;
-  days: [number];
+  days: number[];
 }
 
 interface Result { 
@@ -13,10 +15,25 @@ interface Result {
   average: number;
 }
 
+const parseArguments = (args: string[]): exerciseValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  // if (args.length > 4) throw new Error('Too many arguments');
+  // console.log(args.slice(2));
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      target: Number(args[2]),
+      days: args.slice(3).map(Number)
+    };
+  } else {
+    throw new Error('Provided values were not numbers!');
+  }
+}
+
 const calculateExercises = (target: number, days: number[]) : Result => {
   let trainingDays: number = 0;
   let trainingTime: number = 0;
   let average: number = 0;
+  let desc: string = 'not too bad but could be better';
 
   days.map(day => {
     if (day > 0) {
@@ -24,18 +41,41 @@ const calculateExercises = (target: number, days: number[]) : Result => {
       trainingTime += day
     }
   })
+
   average = trainingTime / days.length;
-  console.log(average, trainingDays, trainingTime);
+  const rating: ratingRange = average > target + 0.5 ? 3 : average < target - 0.5 ? 1 : 2;
+  if (rating === 1) {
+    desc = 'NOT great Bob!';
+  } else if (rating === 3) {
+    desc = 'GREAT Bob!'
+  }
+
+  // console.log(average, trainingDays, trainingTime, rating, desc);
 
   return {
     periodLength: days.length,
     trainingDays: trainingDays,
     success: average > target,
-    rating: 2,
-    ratingDescription: 'jee',
+    rating: rating,
+    ratingDescription: desc,
     target: target,
     average: average
   }
 }
 
-console.log(calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]))
+// console.log(calculateExercises(2, [3, 0, 2, 4.5, 0, 3, 1]))
+// console.log(calculateExercises(2, [1, 0, 2, 4.5, 0, 3, 1, 0, 4]));
+try {
+  const { target, days } = parseArguments(process.argv);
+  // console.log(height, mass)
+  console.log(calculateExercises(target, days));
+} catch (error: unknown) {
+  let errorMessage = 'Something went wrong: '
+  if (error instanceof Error) {
+    errorMessage += error.message;
+  }
+  console.log(errorMessage);
+}
+
+
+export {}
