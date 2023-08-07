1f213cac435b2adef3f8e049676831e43dd17681
Daniele Castagna <daniele.castagna@outlook.com>
2023-08-07 11:39:57 +0300

    9.16, 9.17, 9.18alku

--- osa9/ilarisflightdiaries/frontend/src/App.tsx
+++ osa9/ilarisflightdiaries/frontend/src/App.tsx
@@ -1,34 +1,78 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import { Diary, Weather, Visibility } from './types';
+import axios from 'axios';
 
-interface Note {
-  id: number,
-  content: string
+const Notification = (props: { error: string }) => {
+
+  return <div>{props.error}</div>
 }
 
+
 const App = () => {
-  const [notes, setNotes] = useState<Note[]>([]);
-  const [newNote, setNewNote] = useState('');
+  const [diaries, setDiaries] = useState<Diary[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newVisibility, setNewVisibility] = useState<Visibility | string>('');
+  const [newWeather, setNewWeather] = useState<Weather | string>('');
+  const [newComment, setNewComment] = useState('');
+  const [notification, setNotification] = useState('');
 
+  useEffect(() => {
 
-  const noteCreation = (event: React.SyntheticEvent) => {
+    axios.get<Diary[]>('http://localhost:3001/api/diaries').then(response => {
+      console.log(response.data);
+      setDiaries(response.data);
+    })
+  }, [])
+
+  const diaryCreation = async (event: React.SyntheticEvent) => {
     event.preventDefault()
-    const noteToAdd = {
-      content: newNote,
-      id: notes.length + 1
+    const diaryToAdd = {
+      date: newDate,
+      visibility: newVisibility,
+      weather: newWeather,
+      comment: newComment,
+      id: diaries.length + 1
     }
-    setNotes(notes.concat(noteToAdd));
-    setNewNote('')
+
+    try {
+      await axios.post<Diary>('http://localhost:3001/api/diaries', diaryToAdd).then(response => {
+        console.log(response);
+      })
+      setDiaries(diaries.concat(diaryToAdd as Diary));
+      setNewDate('');
+      setNewVisibility('');
+      setNewWeather('');
+      setNewComment('');
+    } catch (error) {
+      if (axios.isAxiosError(error)) {
+        console.log(error.status)
+        console.error(error.response);
+      } else {
+        console.error(error);
+      }
+    }
   };
 
   return (
     <div>
-      <form onSubmit={noteCreation}>
-        <input value={newNote} onChange={(event) => setNewNote(event.target.value)} />
+      <Notification error={notification} />
+      <h2>Add new entry</h2>
+      <form onSubmit={diaryCreation}>
+        date <input value={newDate} onChange={(event) => setNewDate(event.target.value)} /><br/>
+        visibility <input value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)} /><br/>
+        weather <input value={newWeather} onChange={(event) => setNewWeather(event.target.value)} /><br/>
+        comment <input value={newComment} onChange={(event) => setNewComment(event.target.value)} /><br/>
         <button type='submit'>add</button>
       </form>
+
+      <h2>Diary entries</h2>
       <ul>
-        {notes.map(note =>
-          <li key={note.id}>{note.content}</li>
+        {diaries.map(diary =>
+          <li key={diary.id}>
+            <h3>{diary.date}</h3>
+            visibility: {diary.visibility}<br/>
+            weather: {diary.weather}
+          </li>
         )}
       </ul>
     </div>
--- osa9/ilarisflightdiaries/frontend/src/types.ts
+++ osa9/ilarisflightdiaries/frontend/src/types.ts
@@ -1,6 +1,24 @@
-export interface Note {
-  id: number,
-  content: string
+export enum Weather {
+  Sunny = 'sunny',
+  Rainy = 'rainy',
+  Cloudy = 'cloudy',
+  Stormy = 'stormy',
+  Windy = 'windy',
 }
 
-export type NewNote = Omit<Note, 'id'>
+export enum Visibility {
+  Great = 'great',
+  Good = 'good',
+  Ok = 'ok',
+  Poor = 'poor',
+}
+
+export interface Diary {
+  id: number,
+  date: string,
+  weather: Weather,
+  visibility: Visibility,
+  comment: string,
+}
+
+export type NewDiary = Omit<Diary, 'id'>
