80e16e71b79c99a0981ce6a2f1f96365789f8aef
Daniele Castagna <daniele.castagna@outlook.com>
2023-12-18 15:18:09 +0200

    12.5-12.7alku

--- osa12/todo-app/todo-backend/routes/todos.js
+++ osa12/todo-app/todo-backend/routes/todos.js
@@ -35,7 +35,8 @@ singleRouter.delete('/', async (req, res) => {
 
 /* GET todo. */
 singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+  res.send(req.todo);
+  // res.sendStatus(200); // Implement this
 });
 
 /* PUT todo. */
