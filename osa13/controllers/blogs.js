a64f1e25483402a52f941070e1c7410b564330bc
Daniele Castagna <daniele.castagna@outlook.com>
2024-01-18 14:06:16 +0200

    13.13-13.15

--- osa13/controllers/blogs.js
+++ osa13/controllers/blogs.js
@@ -3,6 +3,7 @@ const jwt = require('jsonwebtoken')
 const { SECRET } = require('../util/config')
 
 const { Blog, User } = require('../models')
+const { Op, Sequelize } = require('sequelize')
 
 const blogFinder = async (req, res, next) => {
   req.blog = await Blog.findByPk(req.params.id)
@@ -26,12 +27,39 @@ const tokenExtractor = (req, res, next) => {
 }
 
 router.get('/', async (req, res) => {
+  let where = {}
+
+  if (req.query.search) {
+    where = {
+      [Op.or]: [
+        {
+          title: {
+            [Op.iLike]: `%${req.query.search}%`
+          }
+        },
+        {
+          author: {
+            [Op.iLike]: `%${req.query.search}%`
+          }
+        }
+      ]
+    }
+    // where.title = {
+    //   [Op.iLike]: `%${req.query.search}%`
+    // }
+    // where.author = {
+    //   [Op.iLike]: `%${req.query.search}%`
+    // }
+  }
+
   const blogs = await Blog.findAll({
+    order: [['likes', 'DESC']],
     attributes: { exclude: ['userId'] },
     include: {
       model: User,
       attributes: ['name']
-    }
+    },
+    where
   })
   res.json(blogs)
 })
